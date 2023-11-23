3ef9f4b4d7db08b791f1c97b59d670f774c8456d
Murilo Moutinho da Silva <104277785+MuriloMoutinho@users.noreply.github.com>
2023-11-23 07:47:01 -0300

    register user

---
+++ IFinder/src/IFinder.Application/Contracts/Documents/Dtos/RegisterUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFinder.Application.Contracts.Documents.Dtos
+{
+    public class RegisterUserDto
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Token { get; set; }
+    }
+}
--- IFinder/src/IFinder.Application/Contracts/Documents/Requests/User/CreateUserRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IFinder.Application.Contracts.Documents.Requests.User;
-
-public class CreateUserRequest
-{
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
-
